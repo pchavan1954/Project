3ec0d66f72c9b40488aa770782eaae6c017e392d
pchavan1954 <ptchavan99@gmail.com>
2022-12-28 21:29:50 +0530

    new code is added

---
+++ src/com/demo/HashMapDemo.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+	Map<String,Integer> map = new HashMap<String,Integer>();
+	map.put("ajay", 1);
+	map.put("vijay",2);
+	map.put("Sanjay",3);
+	map.put("prathamesh", 4);
+	map.forEach((x,y)->System.out.println(x+ " " + y));
+	}
+}
