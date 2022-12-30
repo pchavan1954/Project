d8f2bcddbffb2d7d63b8a13950fab969160c8039
pchavan1954 <121347408+pchavan1954@users.noreply.github.com>
2022-12-30 14:25:11 +0530

    Update jump.java

--- src/com/demo/jump.java
+++ src/com/demo/jump.java
@@ -6,6 +6,8 @@ public class jump {
   public static void main(String[] args) {
 		
 		String str = "Hello all good morning";
+	             str = "hii prathamesh how are you";
+		     str = " are you free tomorrow";
 		
 		System.out.println(str);
 	}
