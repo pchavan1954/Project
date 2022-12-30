2441d8a0edfbed41faa69d7c7635ba8973ccd2ae
pchavan1954 <121347408+pchavan1954@users.noreply.github.com>
2022-12-30 14:38:21 +0530

    Update demo.java

--- src/com/demo/demo.java
+++ src/com/demo/demo.java
@@ -1,6 +1,14 @@
 public class Demo{
   
  // here we are adding the new code again 
+   public static void main(String[] args) {
+		
+		
+	          String  str = "hii prathamesh how are you";
+		 
+		
+		System.out.println(str);
+	}
   
   
   
