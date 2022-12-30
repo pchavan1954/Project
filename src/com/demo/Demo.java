c9af6ff991d8ea9a0a3929b251fc2168d8fc06cd
pchavan1954 <ptchavan99@gmail.com>
2022-12-30 14:45:25 +0530

    mock

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Demo{
+  public static void main(String[] args) {
+	
+	  System.out.println("hello good evening");
+}
+ // here we are adding the new code again 
+  
+  
+  
+}
--- src/com/demo/demo.java
+++
@@ -1,7 +0,0 @@
-public class Demo{
-  
- // here we are adding the new code again 
-  
-  
-  
-}
