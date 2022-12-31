fa6c6f481ec21c1b26648f357c2b9ce27559a47b
pchavan1954 <ptchavan99@gmail.com>
2022-12-31 13:24:16 +0530

    adding the code

--- src/com/sample/Shine.java
+++ src/com/sample/Shine.java
@@ -5,7 +5,7 @@ public class Shine {
 	public static void main(String[] args) {
 		
 		String str = "Hello all good morning";
-<<<<<<< HEAD
+
 
 		
 		System.out.println(	"Hello all good night");
@@ -13,14 +13,14 @@ public class Shine {
 
 		System.out.println("Hello all evening");
 		System.out.println(str);
-=======
+
 		
 		System.out.println("Hello all good night");
 		
 		System.out.println("Hello all evening");
 		System.out.println(str);
 
->>>>>>> e10c2cf22f54c77a31dbc1fd055be329cce623a4
+
 	}
 
 }
