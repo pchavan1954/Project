652b2014aad2617b5ee62f66a64f7d60f91c6ff1
pchavan1954 <ptchavan99@gmail.com>
2022-12-31 14:24:46 +0530

    new code is added

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class Demo{
+  
+ // here we are adding the new code again 
+   public static void main(String[] args) {
+		
+		
+	          String  str = "hii prathamesh how are you";
+		 
+
+	    System.out.println("Hello guys we are reday for run");
+
+		System.out.println("Hello all");
+		System.out.println(str);
+	}
+  
+  
+  
+}
